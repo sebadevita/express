0e0cbc1ed9856faac94076c04380ed6bb7f71128
Sebastian De Vita <sebassdevita@gmail.com>
2022-02-18 16:40:30 -0300

    Ejemplo primeras rutas creadas

--- api-express/routes/index.js
+++ api-express/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const router = express.Router()
+const users = require("../public/data/users.json")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" })
+})
 
-module.exports = router;
+router.get("/users", (req, res) => {
+  res.json(users)
+})
+
+router.get("/users/quantity", (req, res) => {
+  res.json(users.length)
+})
+
+module.exports = router
